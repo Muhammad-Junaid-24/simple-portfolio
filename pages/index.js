834f23d2c08f8179bc822bd858ee11f7cb964ad1
Muhammad Junaid <junaid@epochclan.com>
2023-04-13 23:59:21 +0500

    update readme.md and add hoobank project

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 import frontend from "../public/frontend.png";
 import backend from "../public/backend.png";
+import hoobank from "../public/hoobank.png";
+import jira from '../public/jira.png'
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -36,14 +38,6 @@ export default function Home() {
                   onClick={() => setDarkMode(!darkMode)}
                   className=" cursor-pointer text-2xl"
                 />
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
               </li>
             </ul>
           </nav>
@@ -105,7 +99,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">MongoDB</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
+              <Image src={jira} width={150} height={100} className="object-contain"/>
               <h3 className="text-lg font-semibold pt-8 pb-2 text-teal-600">
                 Miscellaneous
               </h3>
@@ -120,21 +114,28 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
+              Take a look at some of the cool projects I've made
             </p>
           </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web1}
-              />
+          <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap items-center">
+            <div className="max-w-[370px]">
+              <Image className="rounded-md object-contain" fill src={hoobank} />
+              <div className="w-full flex justify-center gap-x-4 mt-6">
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md hover:scale-110 transition"
+                  href="https://hoobank-by-junaid.netlify.app/"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md hover:scale-110 transition"
+                  href="https://github.com/Muhammad-Junaid-24/bank-app-frontend"
+                >
+                  Github Repo
+                </a>
+              </div>
             </div>
           </div>
         </section>
